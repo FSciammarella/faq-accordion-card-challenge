184ead4d6dfe9e3598ecfcebf5270e383f1f9d63
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-03-17 02:20:59 -0300

    deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/faq-accordion-card-challenge/',
-  plugins: [vue()],
-});
